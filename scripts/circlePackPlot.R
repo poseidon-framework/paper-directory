9b4d420d4acbe0bf02c86e2a2a1754cd0df88c2e
Clemens Schmid <clemens@nevrome.de>
2025-10-31 22:11:27 +0100

    and more

--- scripts/circlePackPlot.R
+++ scripts/circlePackPlot.R
@@ -16,8 +16,8 @@ hu <- hu %>%
   dplyr::mutate(
     id = 1:dplyr::n(),
     type = dplyr::case_when(
-      community_archive & aadr_archive ~ "both",
-      community_archive ~ "PCA",
+      (community_archive | minotaur_archive) & aadr_archive ~ "all",
+      (community_archive | minotaur_archive) ~ "PCA or PMA",
       aadr_archive ~ "PAA",
       .default = "none"
     )
@@ -29,9 +29,9 @@ grid_pos <- ggpointgrid::arrange_points_on_grid(
     x = seq(
       min(as.double(hu$publication_date)),
       max(as.double(hu$publication_date)),
-      by = 100
+      by = 50
     ),
-    y = seq(-1000,1000,50)
+    y = seq(-1000,1000,100)
   ) %>% as.matrix,
   pts_xy = hu %>%
     dplyr::transmute(
@@ -45,27 +45,34 @@ grid_pos <- ggpointgrid::arrange_points_on_grid(
 xyz <- tibble::tibble(
     x = as.double(hu$publication_date),#grid_pos[,1],
     y = grid_pos[,2],
-    n = hu$nr_adna_samples * 200
+    n = hu$nr_adna_samples * 350
   )
 center_and_radius <- packcircles::circleRepelLayout(
   xyz,
-  xlim = range(xyz$x),# ylim = c(0,3000),
+  xlim = c(min(xyz$x), max(xyz$x) + 110),
   wrap = FALSE
 )$layout
 polygons <- packcircles::circleLayoutVertices(center_and_radius)
 
-flu <- hu 
-
-ggplot() +
+p <- ggplot() +
   geom_polygon(
     data = hu %>% dplyr::left_join(polygons, by = "id"),
     mapping = aes(as.Date(x), y, group = id, fill = type),
-    colour="black"
+    linewidth = 0.4
   ) +
+  scale_fill_manual(
+    values = c(
+      "all" = "#5785C1",
+      "PAA" = "#CB7A5C",
+      "PCA or PMA" = "#FBA82E",
+      "none" = "#D3D4D8"
+    )
+  ) +
+  guides(fill = guide_legend(title = "Archives")) +
   geom_text(
     data = hu %>% dplyr::bind_cols(
         center_and_radius %>% dplyr::transmute(x_center = x, y_center = y)
-      ) %>% dplyr::filter(nr_adna_samples > 150),
+      ) %>% dplyr::filter(nr_adna_samples > 160),
     mapping = aes(
       x = as.Date(x_center),
       y = y_center,
@@ -76,7 +83,7 @@ ggplot() +
         sep = "\n"
       )
     ),
-    colour="black", size = 3
+    colour="black", size = 1.9
   ) +
   coord_fixed() +
   theme_bw() +
@@ -85,11 +92,24 @@ ggplot() +
   ) +
   theme(
     panel.grid = element_blank(),
+    panel.grid.major.x = element_line(colour = "lightgrey"),
     panel.border = element_blank(),
     axis.line.x = element_line(),
     axis.title = element_blank(),
     axis.text.y = element_blank(),
     axis.ticks.y = element_blank(),
-    legend.position = "bottom"
+    legend.position = c(0.1, 0.8),
+    legend.box.background = element_rect(colour = "black")
   )
+
+ggsave(
+    "test.png",
+    plot = p,
+    device = "png",
+    scale = 1.8,
+    dpi = 300,
+    width = 1400, height = 800, units = "px",
+    limitsize = F,
+    bg = "white"
+)
 
