63936e59687471b510929e2808306457f6b79759
Clemens Schmid <clemens@nevrome.de>
2025-11-10 21:41:08 +0100

    experiments with a size legend

--- scripts/plot_diachronic.R
+++ scripts/plot_diachronic.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 #### read data ####
 
-paper_directory_raw <- readr::read_csv("~/agora/paper-directory/docs/paper_directory.csv")
+paper_directory_raw <- readr::read_csv("../docs/paper_directory.csv")
 
 # fix one entry where the PCA is missing a correct DOI entry
 # can be removed eventually
@@ -17,11 +17,11 @@ paper_directory <- paper_directory_raw %>%
   dplyr::mutate(
     type = dplyr::case_when(
       # (community_archive | minotaur_archive) & aadr_archive ~ "all",
-      (community_archive | minotaur_archive) ~ "PCA or PMA",
-      aadr_archive ~ "PAA",
+      (community_archive | minotaur_archive) ~ "community- or minotaur-archive",
+      aadr_archive ~ "aadr-archive",
       .default = "none"
     ) %>%
-      factor(levels = c("PCA or PMA", "PAA", "none"))
+      factor(levels = c("community- or minotaur-archive", "aadr-archive", "none"))
   ) %>%
   dplyr::arrange(type) %>%
   dplyr::mutate(
@@ -49,7 +49,7 @@ p_bar <- paper_directory %>%
   scale_x_continuous(breaks = 2010:2025) +
   theme_bw() +
   theme(axis.title.x = element_blank()) +
-  ylab("# of published ancient individuals")
+  ylab("# of published ancient genomes")
 
 ggsave(
   "barplot.png",
@@ -83,6 +83,8 @@ grid_pos <- ggpointgrid::arrange_points_on_grid(
 
 # plot(grid_pos[,1],grid_pos[,2])
 
+
+
 # create and arrange circles
 xyz <- tibble::tibble(
     x = as.double(paper_directory$publication_date), # grid_pos[,1],
@@ -94,10 +96,41 @@ center_and_radius <- packcircles::circleRepelLayout(
   xlim = c(min(xyz$x), max(xyz$x) + 120),
   wrap = FALSE
 )$layout
+# paper_directory %>%
+#   dplyr::bind_cols(center_and_radius) %>%
+#   ggplot() +
+#   geom_point(aes(x = nr_adna_samples, y = pi*radius^2))
+# paper_directory %>%
+#   dplyr::bind_cols(center_and_radius) %>%
+#   ggplot() +
+#   geom_point(aes(x = sqrt(nr_adna_samples/pi), y = radius))
 polygons <- packcircles::circleLayoutVertices(center_and_radius)
 
 # assemble plots
 p_no_legend <- ggplot() +
+  # for a fake size legend
+  geom_point(
+    data = tibble::tibble(x = lubridate::ymd("2012-01-01"), y = 0, area = 0),
+    mapping = aes(x, y, size = area), colour = NA
+  ) +
+  scale_radius(
+    # this range must be carefully calibrated to the plot size
+    # this is only a hack to simulate a size legend for the bubble polygons
+    range = c(0,25),
+    transform = scales::trans_new(
+      name = "area_to_radius",
+      transform = function(a) sqrt(a / pi),
+      inverse   = function(r) pi * r^2,
+      domain    = c(0, Inf)
+    ),
+    limits = c(10, 1000),
+    breaks = c(50, 100, 200),
+    guide = guide_legend(
+      title = "# ancient genomes",
+      override.aes = list(color = "#D3D4D8"),
+      direction = "horizontal"
+    )
+  ) +
   geom_polygon(
     data = paper_directory %>% dplyr::left_join(polygons, by = "id"),
     mapping = aes(as.Date(x), y, group = id),
@@ -132,7 +165,11 @@ p_no_legend <- ggplot() +
     axis.line.x = element_line(),
     axis.title = element_blank(),
     axis.text.y = element_blank(),
-    axis.ticks.y = element_blank()
+    axis.ticks.y = element_blank(),
+    legend.position = c(0.2, 0.2),
+    legend.box.background = element_rect(colour = "black"),
+    legend.text = element_text(size = 8),
+    legend.title = element_text(size = 8)
   )
 
 ggsave(
@@ -155,8 +192,8 @@ p <- ggplot() +
   scale_fill_manual(
     values = c(
       #"PCA or PMA" = "#5785C1",
-      "PCA or PMA" = "#00b2ff",
-      "PAA" = "#ffa800",
+      "community- or minotaur-archive" = "#00b2ff",
+      "aadr-archive" = "#ffa800",
       "none" = "#D3D4D8"
     )
   ) +
@@ -192,8 +229,9 @@ p <- ggplot() +
     axis.title = element_blank(),
     axis.text.y = element_blank(),
     axis.ticks.y = element_blank(),
-    legend.position = c(0.1, 0.2),
-    legend.box.background = element_rect(colour = "black")
+    legend.position = c(0.2, 0.2),
+    legend.box.background = element_rect(colour = "black"),
+    legend.text = element_text(size = 8)
   )
 
 ggsave(
